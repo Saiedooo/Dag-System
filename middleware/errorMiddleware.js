426dfedcfdf87d1b7f9233cb0113cfc8aeb18bad
saiedooo <hemasaied2003@gmail.com>
2025-12-04 02:44:39 +0200

    some changes

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -41,16 +41,16 @@ const globalError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
+  // Handle specific MongoDB errors (in both dev and prod)
+  if (err.name === 'CastError') err = handleCastErrorDB(err);
+  if (err.code === 11000) err = handleDuplicateFieldsDB(err);
+  if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
+  if (err.name === 'JsonWebTokenError') err = handleJwtInvalidSignature();
+  if (err.name === 'TokenExpiredError') err = handleJwtExpired();
+
   if (process.env.NODE_ENV === 'development') {
     sendErrorForDev(err, res);
   } else {
-    // Handle specific MongoDB errors
-    if (err.name === 'CastError') err = handleCastErrorDB(err);
-    if (err.code === 11000) err = handleDuplicateFieldsDB(err);
-    if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
-    if (err.name === 'JsonWebTokenError') err = handleJwtInvalidSignature();
-    if (err.name === 'TokenExpiredError') err = handleJwtExpired();
-
     sendErrorForProd(err, res);
   }
 };
