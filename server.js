9e59abb979b382cee999da69c67a92833ba90b12
saiedooo <hemasaied2003@gmail.com>
2025-12-29 23:37:16 +0200

    some changes

--- server.js
+++ server.js
@@ -25,22 +25,19 @@ process.on('uncaughtException', (err) => {
 const app = express();
 
 // Enable CORS before other middleware
-// app.use(
-//   cors({
-//     origin: [
-//       'http://localhost:5173',
-//       'http://localhost:3000',
-//       'http://localhost:5174',
-//     ],
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-//     exposedHeaders: ['Authorization'],
-//   })
-// );
-
-// Handle preflight requests explicitly
-app.options('*', cors());
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'http://localhost:5174',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    exposedHeaders: ['Authorization'],
+  })
+);
 
 // parse JSON bodies
 app.use(express.json());
